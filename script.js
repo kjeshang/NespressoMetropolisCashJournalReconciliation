a6cec1efcdb7afd04fef77bf4721b44582ea4e68
kjeshang <kunal@zypgallery.ca>
2024-04-04 17:43:01 -0700

    Finished basic opening & closing cash reconciliation

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+// *** Initialization Variables***
 document.getElementById("creditDebitReconciliation").style.display = "none";
+document.getElementById("open_cashReconciliation").style.display = "none";
+document.getElementById("close_cashReconciliation").style.display = "none";
 
+// *** Reset Webpage ***
+function reset(){
+    location.reload();
+}
+
+// *** Toggle between Cash and Credit Reconciliation ***
 document.getElementById("toggle_cashReconciliation").addEventListener("click", () => {
     document.getElementById("creditDebitReconciliation").style.display = "none";
     document.getElementById("cashReconciliation").style.display = "block";
@@ -10,12 +19,25 @@ document.getElementById("toggle_creditDebitReconciliation").addEventListener("cl
     document.getElementById("creditDebitReconciliation").style.display = "block";
 });
 
+// *** Toggle Opening & Closing Summaries for Cash Reconciliation ***
+document.getElementById("toggle_openCashReconciliation").addEventListener("click", () => {
+    document.getElementById("open_cashReconciliation").style.display = "block";
+    document.getElementById("close_cashReconciliation").style.display = "none";
+});
+
+document.getElementById("toggle_closeCashReconciliation").addEventListener("click", () => {
+    document.getElementById("close_cashReconciliation").style.display = "block";
+    document.getElementById("open_cashReconciliation").style.display = "none";
+});
+
+// CASH RECONCILIATION --------------------------------------------------------------------------------------------------
+
 var denomination_list = [
     "hundred","fifty","twenty","ten","five",
     "two","one","quarter","dime","nickel"
 ];
 
-var tbody = document.getElementById("tbody");
+var tbody = document.getElementById("tbody_cash");
 var trElements = tbody.getElementsByTagName("tr");
 var numberOfTR = trElements.length;
 console.log("Number of <tr> elements in <tbody>: " + numberOfTR);
@@ -26,19 +48,9 @@ function calculateCashValue(base_amount){
     var count_id = base_amount + "_count";
     var value_id = base_amount + "_value";
 
-    // console.log(denomination_id);
-    // console.log(count_id);
-    // console.log(value_id);
-
     var denomination = parseFloat(document.getElementById(denomination_id).innerHTML);
-    // console.log(denomination);
     var count = parseFloat(document.getElementById(count_id).value);
-    // console.log(count);
-    
     var product_value = denomination * count;
-
-    console.log(value_id);
-    console.log(product_value);
 
     if(isNaN(product_value)){
         document.getElementById(value_id).innerHTML = 0;
@@ -46,22 +58,117 @@ function calculateCashValue(base_amount){
     else{
         document.getElementById(value_id).innerHTML = product_value.toString();
     }
+}
 
-    console.log(value_id);
-    console.log(product_value);
+// *** Outcome of Cash Reconciliation ***
+function finalizeCashOutcome(shift){
+    // var total_amount = parseFloat(document.getElementById("total_amount").innerHTML.toString());
+    if(shift == "open"){
+        // console.log(shift);
+        var total_amount = parseFloat(document.getElementById("total_amount_open").innerHTML.toString());
+        // console.log(total_amount);
+        var cashJournalAmount = parseFloat(document.getElementById("cashJournalAmount_open").innerHTML);
+        // console.log(cashJournalAmount);
+        var outcome = "";
+        if(cashJournalAmount > total_amount){
+            outcome = "Short";
+        }
+        else if(cashJournalAmount < total_amount){
+            outcome = "Over";
+        }
+        else{
+            outcome = "Balanced"
+        }
+        document.getElementById("outcome_cashOpen").innerHTML = outcome;
+        // console.log(outcome);
+        difference = Math.abs(total_amount - cashJournalAmount);
+        document.getElementById("difference_cashOpen").innerHTML = difference;
+        // console.log(difference);
+    }
+    else if(shift == "close"){
+        console.log(shift);
+        var total_amount = parseFloat(document.getElementById("total_amount_close").innerHTML.toString());
+        console.log(total_amount);
+        var cashJournalAmount = parseFloat(document.getElementById("cashJournalAmount_close").value);
+        console.log(cashJournalAmount);
+        var outcome = "";
+        if(cashJournalAmount > total_amount){
+            outcome = "Short";
+        }
+        else if(cashJournalAmount < total_amount){
+            outcome = "Over";
+        }
+        else{
+            outcome = "Balanced"
+        }
+        document.getElementById("outcome_cashClose").innerHTML = outcome;
+        console.log(outcome);
+        difference = Math.abs(total_amount - cashJournalAmount);
+        document.getElementById("difference_cashClose").innerHTML = difference.toString();
+        console.log(difference);
+    }
 }
 
 // *** Calculate Total Cash Value ***
-document.getElementById("cash_table").addEventListener("change", () => {
+function calculateTotalCashValue(shift){
     sumTotal = 0;
     for(i=0; i < numberOfTR; i++){
         var value_id = denomination_list[i] + "_value";
         product_value = parseFloat(document.getElementById(value_id).innerHTML);
         if(!isNaN(product_value)){
-            console.log(value_id);
             sumTotal += parseFloat(product_value);
         }
     }
-    console.log(sumTotal);
-    document.getElementById("total_amount").innerHTML = sumTotal.toString();
+    var id = "total_amount_" + shift;
+    document.getElementById(id).innerHTML = sumTotal.toString();
+}
+
+document.getElementById("cash_table").addEventListener("change", () => {
+    // sumTotal = 0;
+    // for(i=0; i < numberOfTR; i++){
+    //     var value_id = denomination_list[i] + "_value";
+    //     product_value = parseFloat(document.getElementById(value_id).innerHTML);
+    //     if(!isNaN(product_value)){
+    //         sumTotal += parseFloat(product_value);
+    //     }
+    // }
+    // document.getElementById("total_amount_open").innerHTML = sumTotal.toString();
+    // document.getElementById("total_amount_close").innerHTML = sumTotal.toString();
+
+    calculateTotalCashValue("open");
+    calculateTotalCashValue("close");
+    for(i=0; i < numberOfTR; i++){
+        var count_id = denomination_list[i] + "_count";
+        calculateTotalCashValue("open");
+        calculateTotalCashValue("close");
+        document.getElementById(count_id).addEventListener("change", finalizeCashOutcome("open"));
+        document.getElementById(count_id).addEventListener("change", finalizeCashOutcome("close"));
+    }
+
+    // document.getElementById("toggle_openCashReconciliation").addEventListener("click", () => {
+    //     for(i=0; i < numberOfTR; i++){
+    //         var count_id = denomination_list[i] + "_count";
+    //         calculateTotalCashValue("open");
+    //         document.getElementById(count_id).addEventListener("change", finalizeCashOutcome("open"));
+    //     }
+    // });
+    // document.getElementById("cash_table").addEventListener("change", finalizeCashOutcome("open"));
+
+    // document.getElementById("toggle_closeCashReconciliation").addEventListener("click", () => {
+    //     for(i=0; i < numberOfTR; i++){
+    //         var count_id = denomination_list[i] + "_count";
+    //         calculateTotalCashValue("close");
+    //         document.getElementById(count_id).addEventListener("change", finalizeCashOutcome("close"));
+    //     }
+    // });
+    // document.getElementById("cash_table").addEventListener("change", finalizeCashOutcome("close"));
 });
+
+document.getElementById("cashJournalAmount_close").addEventListener("change", () =>{
+    calculateTotalCashValue("open");
+    calculateTotalCashValue("close");
+    finalizeCashOutcome("open");
+    finalizeCashOutcome("close");
+});
+
+// CREDIT & DEBIT RECONCILIATION --------------------------------------------------------------------------------------------------
